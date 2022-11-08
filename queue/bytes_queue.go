b27ec78c6189678fdc3f5188b89f39e8fa504b3e
ericluj <847751875@qq.com>
2022-11-08 18:26:31 +0800

    queue

--- queue/bytes_queue.go
+++ queue/bytes_queue.go
@@ -2,22 +2,34 @@ package queue
 
 import "encoding/binary"
 
+const (
+	// Number of bytes to encode 0 in uvarint format
+	minimumHeaderSize = 17 // 1 byte blobsize + timestampSizeInBytes + hashSizeInBytes
+	// Bytes before left margin are not used. Zero index means element does not exist in queue, useful while reading slice from index
+	leftMarginIndex = 1
+)
+
+var (
+	errEmptyQueue       = &queueError{"Empty queue"}
+	errInvalidIndex     = &queueError{"Index must be greater than zero. Invalid index."}
+	errIndexOutOfBounds = &queueError{"Index out of range"}
+)
+
 type BytesQueue struct {
-	capacity     int
+	full         bool
 	array        []byte
+	capacity     int
+	maxCapacity  int
+	head         int
+	tail         int
+	count        int
+	rightMargin  int
 	headerBuffer []byte
+	verbose      bool
 }
 
-func NewBytesQueue(capacity int) *BytesQueue {
-	return &BytesQueue{
-		capacity:     capacity,
-		array:        make([]byte, capacity),
-		headerBuffer: make([]byte, binary.MaxVarintLen32),
-	}
-}
-
-func (q *BytesQueue) Get(index int) ([]byte, error) {
-	return nil, nil
+type queueError struct {
+	message string
 }
 
 // TODO:
@@ -39,6 +51,88 @@ func getNeededSize(length int) int {
 	return length + header
 }
 
+func NewBytesQueue(capacity, maxCapacity int, verbose bool) *BytesQueue {
+	return &BytesQueue{
+		array:        make([]byte, capacity),
+		capacity:     capacity,
+		maxCapacity:  maxCapacity,
+		headerBuffer: make([]byte, binary.MaxVarintLen32),
+		tail:         leftMarginIndex,
+		head:         leftMarginIndex,
+		rightMargin:  leftMarginIndex,
+		verbose:      verbose,
+	}
+}
+
+func (q *BytesQueue) Reset() {
+	q.tail = leftMarginIndex
+	q.head = leftMarginIndex
+	q.rightMargin = leftMarginIndex
+	q.count = 0
+	q.full = false
+}
+
+// Push copies entry at the end of queue and moves tail pointer. Allocates more space if needed.
+// Returns index for pushed data or error if maximum size queue limit is reached.
 func (q *BytesQueue) Push(data []byte) (int, error) {
-	return 0, nil
+	// 获取header+数据的长度
+	neededSize := getNeededSize(len(data))
+
+	if !q.canInsertAfterTail(neededSize) {
+
+	}
+
+	index := q.tail
+
+	q.push(data, neededSize)
+
+	return index, nil
+}
+
+// canInsertAfterTail returns true if it's possible to insert an entry of size of need after the tail of the queue
+func (q *BytesQueue) canInsertAfterTail(need int) bool {
+	if q.full {
+		return false
+	}
+	if q.tail >= q.head {
+		return q.capacity-q.tail >= need
+	}
+	// TODO:
+	// 1. there is exactly need bytes between head and tail, so we do not need
+	// to reserve extra space for a potential empty entry when realloc this queue
+	// 2. still have unused space between tail and head, then we must reserve
+	// at least headerEntrySize bytes so we can put an empty entry
+	return q.head-q.tail == need || q.head-q.tail >= need+minimumHeaderSize
+}
+
+// canInsertBeforeHead returns true if it's possible to insert an entry of size of need before the head of the queue
+func (q *BytesQueue) canInsertBeforeHead(need int) bool {
+	if q.full {
+		return false
+	}
+	if q.tail >= q.head {
+		return q.head-leftMarginIndex == need || q.head-leftMarginIndex >= need+minimumHeaderSize
+	}
+	return q.head-q.tail == need || q.head-q.tail >= need+minimumHeaderSize
+}
+
+func (q *BytesQueue) push(data []byte, len int) {
+	headerEntrySize := binary.PutUvarint(q.headerBuffer, uint64(len))
+	q.copy(q.headerBuffer, headerEntrySize)
+
+	q.copy(data, len-headerEntrySize) // TODO:
+
+	if q.tail > q.head {
+		q.rightMargin = q.tail
+	}
+	if q.tail == q.head {
+		q.full = true
+	}
+
+	q.count++
+}
+
+// data中长度为len的字节拷贝到q.array
+func (q *BytesQueue) copy(data []byte, len int) {
+	q.tail += copy(q.array[q.tail:], data[:len])
 }
